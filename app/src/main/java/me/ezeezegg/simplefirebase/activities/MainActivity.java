0a0c6328920fe44e0e260e4da11575f2c88d64f1
Ezequiel García <ezeezegg@gmail.com>
2015-11-06 10:28:34 -0600

    resolver problem with resourse R

--- app/src/main/java/me/ezeezegg/simplefirebase/activities/MainActivity.java
+++ app/src/main/java/me/ezeezegg/simplefirebase/activities/MainActivity.java
@@ -1,30 +1,29 @@
 package me.ezeezegg.simplefirebase.activities;
 
-import android.app.Application;
 import android.content.SharedPreferences;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-
-import com.firebase.client.Firebase;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.Random;
 
 import me.ezeezegg.simplefirebase.R;
-
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 
-    private void setupUsername(){
-        SharedPreferences prefs =  getApplication().getSharedPreferences("ToDoPrefs", 0);
+    private void setupUsername() {
+        SharedPreferences prefs = getApplication().getSharedPreferences("ToDoPrefs", 0);
         String username = prefs.getString("username", null);
-        if (username == null){
+        if (username == null) {
             Random r = new Random();
-            username = "AndroidUser" + r.nextInt(10000);
+            username = "AndroidUser" + r.nextInt(100000);
             prefs.edit().putString("username", username).commit();
         }
     }
