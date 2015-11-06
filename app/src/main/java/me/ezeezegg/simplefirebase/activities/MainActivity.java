6d229a82c1fb8474afa92f8f40cb24403b0f8d66
Ezequiel García <ezeezegg@gmail.com>
2015-11-06 10:39:31 -0600

    init variables and assign SharedPrefrences

--- app/src/main/java/me/ezeezegg/simplefirebase/activities/MainActivity.java
+++ app/src/main/java/me/ezeezegg/simplefirebase/activities/MainActivity.java
@@ -3,19 +3,42 @@ package me.ezeezegg.simplefirebase.activities;
 import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.Firebase;
+import com.firebase.ui.FirebaseRecyclerViewAdapter;
 
 import java.util.Random;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import me.ezeezegg.simplefirebase.R;
+import me.ezeezegg.simplefirebase.ToDoApplication;
 
 public class MainActivity extends ActionBarActivity {
+
+    @Bind(R.id.recycler_view_items) RecyclerView recyclerView;
+    @Bind(R.id.editTextItem) EditText editTestItem;
+
+    private ChildEventListener toDoItemListener;
+    private FirebaseRecyclerViewAdapter adapter;
+    private Firebase dataReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setupUsername();
+        SharedPreferences prefs = getApplication().getSharedPreferences("ToDoPrefs", 0);
+        String username = prefs.getString("username", null);
+        setTitle(username);
+        ToDoApplication app = (ToDoApplication)getApplicationContext();
+        dataReference = app.getFirebase();
     }
 
     private void setupUsername() {
