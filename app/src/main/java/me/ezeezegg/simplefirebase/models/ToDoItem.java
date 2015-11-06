06ec53eff74e7df2e39f97190ac1ba4dac5e1cb4
Ezequiel García <ezeezegg@gmail.com>
2015-11-06 07:26:32 -0600

    create model ToDoItem

---
+++ app/src/main/java/me/ezeezegg/simplefirebase/models/ToDoItem.java
@@ -0,0 +1,40 @@
+package me.ezeezegg.simplefirebase.models;
+
+/**
+ * Created by egarcia on 11/6/15.
+ */
+public class ToDoItem {
+    String username;
+    String item;
+    boolean completed;
+
+    public ToDoItem(String username, String item, boolean completed){
+        this.username = username;
+        this.item = item;
+        this.completed = completed;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
